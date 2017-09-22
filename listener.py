03fb8b045ca865182ea2f213e716aef52653c0f9
TheGreenYonder <32165033+TheGreenYonder@users.noreply.github.com>
2017-09-22 10:02:29 +0200

    adjust to new make_request method
    make_request now wants the url als parameter.
    KeyboardInterrupt added so the server will shut down when I'm stopping listener while testing

--- listener.py
+++ listener.py
@@ -13,16 +13,23 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("", 8000))
 s.listen(5)
 
+#create Handler object
 my_handler = Handler()
 logging.info("[MAIN]\nBinding on all interfaces on port 8000\nObject initialized\nWaiting for connection...\n")
 
+loop = True
 #Loop keeps Listener alive
-while True:
+while loop:
 
-    #create object with incoming connection
-    my_handler.read_data(s.accept())
-
-    #calls the function from the devices dict
-    my_handler.make_request()
-
-    logging.info("[MAIN]\nWaiting for new connection...\n")
+    #get the desired url from read_data, give it to make_request
+    try:
+        url = my_handler.read_data(s.accept())
+        my_handler.make_request(url)
+        logging.info("[MAIN]\nWaiting for new connection...\n")
+    except KeyboardInterrupt:
+        #when interrupting while testing, the socket will live for another few seconds - minutes, so let's catch it
+        s.close()
+        loop = False
+        logging.info("[MAIN]\nKeyboardInterrupt - Server shutting down\n")
+    except:
+        logging.warning("Seems like something else went wrong\n")
